9a259ddb7203160376253d6a0bdff54bd11cbcb6
walidkarouach <karouachwalid700@gmail.com>
2026-06-17 17:55:24 +0100

    Ajouter la connexion avec PDO

---
+++ config/config.php
@@ -0,0 +1,28 @@
+<?php
+
+$host = "127.0.0.1";
+$dbname = "gestion_scolaire";
+$user = "root";
+$password = "";
+
+try {
+
+    $pdo = new PDO(
+        "mysql:host=$host;dbname=$dbname;charset=utf8",
+        $user,
+        $password
+    );
+
+    $pdo->setAttribute(
+        PDO::ATTR_ERRMODE,
+        PDO::ERRMODE_EXCEPTION
+    );
+
+    echo "Connexion réussie !";
+
+} catch(PDOException $e){
+
+    die("Erreur de connexion : " . $e->getMessage());
+
+}
+?>
